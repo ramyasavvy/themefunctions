9c15cc56869f3b448725cbe50d876f4af727d10e
ramyasavvy <ramya@olivela.com>
2025-11-03 14:37:30 +0530

    Add files via upload

---
+++ test-fakeapi-migration.js
@@ -0,0 +1,23 @@
+const { fakeapisetupmigration } = require("./utils/general/fakeapisetupmigration");
+
+exports.handler = async (event) => {
+  try {
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    };
+    const shopifyResponse = await fakeapisetupmigration();
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(shopifyResponse),
+    }
+  } catch (error) {
+        return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
